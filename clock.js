9ac2a89751b09b71d05b438d75edd81f41426869
ShripadJagtap <shripadjagtapyt@gmail.com>
2021-07-16 22:09:12 +0530

    Completed Js files and the whole project

--- clock.js
+++ clock.js
@@ -1,12 +1,26 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.msGetRegionContent("2d");
-var radius = canvas.height / 2;
-ctx.translate(radius, radius);
-radius = radius * 0.90
-drawClock();
+// Constants that won't change through the script
+// #hour - target id for hour hand in the svg
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
 
-function drawClock(){
-    ctx.arc(0,0,radius,0,2*Math.PI);
-    ctx.fillstyle = "White";
-    ctx.fill();
+
+function run_the_clock(){
+  var date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  console.log("Hour: "+hr+ " Minute: "+ min + " Second: "+ sec);
+
+  let hrPosition = hr*360/12 + ((min * 360/60)/12) ;
+  let minPosition = (min * 360/60) + (sec* 360/60)/60;
+  let secPosition = sec * 360/60;
+
+  //Then we need to apply these numbers as degrees in the inline styles for transform on each of the objects.
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
+
+
+var interval = setInterval(run_the_clock, 1000);
